888a5c6fce330f5d822e236dcfef6fa4f869c99c
engeneeral-cpu <engeneeral@gmail.com>
2026-09-10 02:22:26 +0530

    feat: register company universe API

--- server-entry.js
+++ server-entry.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { registerAuthRoutes } = require('./auth-routes');
+const { registerCompanyRoutes } = require('./company-routes');
 
 const originalListen = express.application.listen;
 express.application.listen = function patchedListen(...args) {
   registerAuthRoutes(this);
+  registerCompanyRoutes(this);
   return originalListen.apply(this, args);
 };
 
