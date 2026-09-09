471cce8b181acf0509e8cb8711284a119296c8b6
engeneeral-cpu <engeneeral@gmail.com>
2026-09-10 02:25:09 +0530

    feat: register live market routes

--- server-entry.js
+++ server-entry.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { registerAuthRoutes } = require('./auth-routes');
 const { registerCompanyRoutes } = require('./company-routes');
+const { registerLiveMarketRoutes } = require('./live-market-routes');
 
 const originalListen = express.application.listen;
 express.application.listen = function patchedListen(...args) {
   registerAuthRoutes(this);
   registerCompanyRoutes(this);
+  registerLiveMarketRoutes(this);
   return originalListen.apply(this, args);
 };
 
